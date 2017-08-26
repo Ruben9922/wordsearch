6d09dcb5ff953719308dda0ba83ba6b534d4c1b0
Ruben9922 <Ruben9922@users.noreply.github.com>
2017-08-26 22:31:29 +0100

    Place words into wordsearch

--- src/WordsearchComponent.js
+++ src/WordsearchComponent.js
@@ -1,31 +1,63 @@
 import React, {Component} from 'react';
 import {List} from 'semantic-ui-react';
 import './WordsearchComponent.css';
+import {Enum} from 'enumify';
 
 class WordsearchComponent extends Component {
   constructor(props) {
     super(props);
 
+    Direction.initEnum(["HORIZONTAL", "VERTICAL", "DIAGONAL_UP", "DIAGONAL_DOWN"]);
+
     this.state = {
       wordsearch: this.generateWordsearch()
     }
+
+    this.generateWordsearch = this.generateWordsearch.bind(this);
+    this.placeWords = this.placeWords.bind(this);
   }
 
   generateWordsearch() {
-    return [
-      ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
-      ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
-      ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
-      ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
-      ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
-      ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
-      ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
-      ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
-      ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
-      ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
-      ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
-      ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"]
-    ];
+    const size = this.props.size;
+    let wordsearch = new Array(size);
+
+    for (let i = 0; i < wordsearch.length; i++) {
+      wordsearch[i] = new Array(size);
+      for (let j = 0; j < wordsearch[i].length; j++) {
+        wordsearch[i][j] = "";
+      }
+    }
+
+    this.placeWords(wordsearch);
+
+    return wordsearch;
+  }
+
+  placeWords(wordsearch) {
+    const size = this.props.size;
+
+    for (let word of this.props.words) {
+      let direction = Direction.enumValues[Math.floor(Math.random() * Direction.enumValues.length)];
+
+      // Both min and max origin values are inclusive
+      let minOriginX = 0;
+      let maxOriginX = (direction === Direction.VERTICAL) ? size - 1 : size - word.length;
+      let minOriginY = (direction === Direction.DIAGONAL_UP) ? word.length - 1 : 0;
+      let maxOriginY = (direction === Direction.DIAGONAL_UP) ? size - 1 : size - word.length;
+
+      let originX = Math.floor(Math.random() * (maxOriginX + 1 - minOriginX)) + minOriginX;
+      let originY = Math.floor(Math.random() * (maxOriginY + 1 - minOriginY)) + minOriginY;
+
+      for (let i = 0; i < word.length; i++) {
+        let letter = word.charAt(i);
+
+        let x = (direction === Direction.VERTICAL) ? originX : originX + i;
+        let y = (direction === Direction.HORIZONTAL) ? originY :
+          ((direction === Direction.DIAGONAL_UP) ? originY - i : originY + i);
+
+        wordsearch[y][x] = letter;
+      }
+    }
   }
 
   render() {
@@ -57,6 +89,9 @@ class WordsearchComponent extends Component {
       </div>
     );
   }
+}
+
+class Direction extends Enum {
 }
 
 export default WordsearchComponent;
