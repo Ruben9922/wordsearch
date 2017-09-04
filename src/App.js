af1fa7f331c40614d44d50975862455facfc523c
Ruben9922 <Ruben9922@users.noreply.github.com>
2017-09-04 13:03:51 +0100

    Remove test input

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 
     this.state = {
       size: 15,
-      words: ["hi"],
+      words: [""],
       allowBackwards: true,
       allowParts: true,
       valid: false
