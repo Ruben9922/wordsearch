b6682f0c0a5a70bffb5ce916e47b0df73e7b233f
Ruben9922 <Ruben9922@users.noreply.github.com>
2019-12-20 22:22:30 +0000

    Initial commit from Create React App

--- src/App.js
+++ src/App.js
@@ -1,146 +1,26 @@
-import React, {Component} from 'react';
-import {Container, Grid, Header, Icon, Menu} from 'semantic-ui-react';
-import FormComponent from "./FormComponent";
-import WordsearchComponent from "./WordsearchComponent";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      parameters: {
-        size: 15,
-        words: [""],
-        allowBackwards: true,
-        allowParts: true
-      },
-      valid: {
-        size: true,
-        words: [false]
-      },
-      errorMessages: {
-        size: [],
-        words: []
-      },
-      submitted: false,
-    };
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.validate = this.validate.bind(this);
-    this.allValid = this.allValid.bind(this);
-  }
-
-  handleChange(name, value) {
-    this.setState(prevState => ({
-      submitted: false,
-      parameters: Object.assign({}, prevState.parameters, {[name]: value})
-    }));
-  }
-
-  handleSubmit() {
-    this.setState({
-      submitted: true
-    }, () => {
-      // Validate form fields
-      this.validate(() => {
-        // Update wordsearch component, only if all form fields are valid (only once state has been updated, hence the callback)
-        if (this.allValid()) {
-          this.wordsearchComponent.update();
-        }
-      });
-    });
-  }
-
-  // TODO: Move back to FormComponent
-  validate(callback) {
-    let valid = {};
-    let errorMessages = {};
-
-    // Size
-    {
-      valid.size = true;
-      errorMessages.size = "";
-
-      let number = Number(this.state.parameters.size);
-      const min = 1;
-      const max = 50;
-      if (!(Number.isInteger(number) && number >= min && number <= max)) {
-        valid.size = false;
-        errorMessages.size = `Size must be an integer between ${min} and ${max} (inclusive)`;
-      }
-    }
-
-    // Words
-    {
-      valid.words = new Array(this.state.parameters.words.length).fill(true);
-      errorMessages.words = new Array(this.state.parameters.words.length).fill("");
-
-      const pattern = /^(\s*[a-zA-Z]+\s*)+$/;
-      for (let [index, word] of this.state.parameters.words.entries()) {
-        if (word === "") {
-          valid.words[index] = false;
-          errorMessages.words[index] = "Word cannot be empty";
-          continue;
-        }
-        if (!pattern.test(word)) {
-          valid.words[index] = false;
-          errorMessages.words[index] = "Word must only consist of letters, optionally separated by spaces (e.g. \"dog\", \"dog food\", \"Rottweiler\")";
-          continue;
-        }
-        if (valid.size && word.length > this.state.parameters.size) {
-          valid.words[index] = false;
-          errorMessages.words[index] = "Length of word cannot exceed size";
-        }
-      }
-    }
-
-    this.setState({
-      valid: valid,
-      errorMessages: errorMessages
-    }, callback);
-  }
-
-  allValid() {
-    return this.state.valid.size === true && this.state.valid.words.every(item => item === true);
-  }
-
-  render() {
-    return (
-      <div>
-        <Menu fixed="top" inverted>
-          <Container fluid>
-            <Menu.Item header link href=".">Wordsearch Generator</Menu.Item>
-            <Menu.Menu position="right">
-              <Menu.Item link href="//rubendougall.co.uk/">
-                <Icon name="arrow left"/>
-                Back to Main Website
-              </Menu.Item>
-              <Menu.Item link href="https://github.com/Ruben9922/wordsearch">
-                <Icon name="github"/>
-                GitHub
-              </Menu.Item>
-            </Menu.Menu>
-          </Container>
-        </Menu>
-        <Container fluid style={{paddingTop: "4em"}}>
-          <Header as="h1" textAlign="center">Wordsearch Generator</Header>
-
-          <Grid stackable padded divided>
-            <Grid.Row>
-              <FormComponent size={this.state.parameters.size} words={this.state.parameters.words} allowBackwards={this.state.parameters.allowBackwards}
-                             allowParts={this.state.parameters.allowParts} onChange={this.handleChange}
-                             onSubmit={this.handleSubmit} submitted={this.state.submitted} valid={this.state.valid} errorMessages={this.state.errorMessages}/>
-
-              <WordsearchComponent size={parseInt(this.state.parameters.size, 10)} words={this.state.parameters.words}
-                                   allowBackwards={this.state.parameters.allowBackwards} allowParts={this.state.parameters.allowParts}
-                                   submitted={this.state.submitted} allValid={this.allValid} ref={input => this.wordsearchComponent = input}/>
-            </Grid.Row>
-          </Grid>
-        </Container>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
